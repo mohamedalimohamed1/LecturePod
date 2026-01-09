702257b12132e4c72bde0dbb20209b64c3eedec1
mohamedalimohamed1 <mazcuud37@gmail.com>
2026-01-09 13:47:16 +0300

    ---

--- js/state.js
+++ js/state.js
@@ -4,7 +4,7 @@ export const state = {
     theme: 'dark',
     lectures: [
         { id: 'lecture-1', title: 'Sistem Analizi - Vize Ve Final', file: 'data/lecture_one.json' },
-        { id: 'lecture-2', title: 'Dijital Okuryazarlık - Vize Sınavı', file: 'data/lecture_six.json' },
+        { id: 'lecture-2', title: 'Dijital Okuryazarlık - Vize Ve Final', file: 'data/lecture_six.json' },
         { id: 'lecture-3', title: 'Görüntü İşleme - Vize Ve Final', file: 'data/lecture_four.json' },
         { id: 'lecture-4', title: 'Kriptoloji ve Bilgi Güvenliği - Vize Sınavı', file: 'data/lecture_seven.json' },
         { id: 'lecture-5', title: 'Uzaktan Algılama - Vize Sınavı', file: 'data/lecture_three.json' },
